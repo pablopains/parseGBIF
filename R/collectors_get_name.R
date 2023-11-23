c6030728f25ba0d600cfc8ce9648bd2497d3c713
Pablo Hendrigo Alves de Melo <pablopains@yahoo.com.br>
2023-11-23 18:55:56 -0300

    collectors_get_name

--- R/collectors_get_name.R
+++ R/collectors_get_name.R
@@ -41,37 +41,40 @@ collectors_get_name <- function(x)
       x = strsplit(x,"\\|")[[1]][1]
    }
 
+
+   x = toupper(x)
+
    x = gsub("COLLECTOR(S): ","",x)
    x = gsub("COLLECTORS: ","",x)
 
    x = gsub("COLLABORATION; ","",x)
-   "PROJETO FLORA CRISTALINO; "
-   "PROJETO FLORA CRISTALINO;"
 
-   "ET AL.; "
-   "ET AL.;"
+   x = gsub("PROJETO FLORA CRISTALINO; ","",x)
+   x = gsub("PROJETO FLORA CRISTALINO;","",x)
 
 
+   x = gsub( "ET AL.; ","",x)
+   x = gsub( "ET AL.;","",x)
 
-   x = gsub("[á|à|â|ã|ä]","a",x)
+   # x = gsub("[á|à|â|ã|ä]","a",x)
    x = gsub("[Á|À|Â|Ã|Ä]","A",x)
 
-   x = gsub("[ó|ò|ô|õ|ö]","o",x)
+   # x = gsub("[ó|ò|ô|õ|ö]","o",x)
    x = gsub("[Ó|Ò|Ô|Õ|Ö]","O",x)
 
-   x = gsub("[í|ì|î|ï]","i",x)
+   # x = gsub("[í|ì|î|ï]","i",x)
    x = gsub("[Í|Ì|Î|Ï]","I",x)
 
-   x = gsub("[ú|ù|û|ü]","u",x)
+   # x = gsub("[ú|ù|û|ü]","u",x)
    x = gsub("[Ú|Ù|Û|Ü]","U",x)
 
-   x = gsub("[é|è|ê|ë]","e",x)
+   # x = gsub("[é|è|ê|ë]","e",x)
    x = gsub("[É|È|Ê|Ë]","E",x)
 
-   x = gsub("ñ","n",x)
+   # x = gsub("ñ","n",x)
    x = gsub("Ñ","N",x)
 
-   x = gsub("ç","c",x)
+   # x = gsub("ç","c",x)
    x = gsub("Ç","C",x)
 
    x = gsub("\\(|\\)"," ",x) # teste pablo 10-02-2020
@@ -80,7 +83,6 @@ collectors_get_name <- function(x)
 
    # 16-11-2023
    {
-     x = toupper(x)
 
      x = gsub("Ć","C",x)
      x = gsub("¡","I",x)
@@ -102,13 +104,10 @@ collectors_get_name <- function(x)
      x = gsub("Ø","O",x)
      x = gsub("Ă","A",x)
      x = gsub("Ę","E",x)
-     x = gsub("  "," ",x)
-
      x = gsub("`","",x)
      x = gsub("’","",x)
 
-
-
+     x = gsub("  "," ",x)
 
    }
 
